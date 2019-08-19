99ae75a5a7eb884f2253d9eb50146e9cba7fdc5f
José Alberto Espinosa <josealbertoespinosa@MacBook-Pro-de-Jose-2.local>
2019-08-19 15:39:12 +0200

    Status first version

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -6,7 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./status.component.scss']
 })
 export class StatusComponent implements OnInit {
+  player = 'Jugador 1';
+  statusGame = 'stopped';
+
+  info = {
+    date: new Date(),
+    duration: 0.0,
+    status: 'waitingGame',
+    pantalla_pulsada: 0,
+    mano_cerrada: 0,
+    distance: 0.0,
+    help: 0,
+    check: 0,
+    successes: 0,
+    failures: 0
+  };
+
   constructor() {}
 
   ngOnInit() {}
+
+  startGame() {
+    this.statusGame = 'running';
+  }
+
+  pauseGame() {
+    this.statusGame = 'paused';
+  }
+
+  continueGame() {
+    this.statusGame = 'running';
+  }
+
+  resetGame() {
+    this.statusGame = 'stopped';
+  }
+
+  stopGame() {
+    this.statusGame = 'stopped';
+  }
 }
